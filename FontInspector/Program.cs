2ca3915e94195078b3ff42a731c30111eff1cacd
Scott Williams <tocsoft@gmail.com>
2023-10-04 13:03:06 +0100

    update to net8.0 rc1

--- FontInspector/Program.cs
+++ FontInspector/Program.cs
@@ -1,5 +1,4 @@
 using FontInspector;
-using Ljbc1994.Blazor.IntersectionObserver;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -11,6 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
-builder.Services.AddIntersectionObserver();
 
 await builder.Build().RunAsync();
